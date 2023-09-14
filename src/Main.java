cf10fd8c9a8ffc7b4f0431bf0e7ff294fd999d8c
jarz <gerhard.jarz@lbs-eibiswald.ac.at>
2023-09-14 14:46:41 +0200

    removed Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         demo myDemo = new demo(100);
-        System.out.println("Hello world!");
 
         System.out.println("Quadrat: "+myDemo.sqr()+"\nWurzel: "+myDemo.sqrt());
     }
